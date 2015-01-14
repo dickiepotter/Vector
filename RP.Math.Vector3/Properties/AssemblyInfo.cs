4de2bc398e433197482204534f39ed514f99e140
Richard Potter <dickiepotter@hotmail.com>
2015-01-14 07:49:24 +0000

    Initial Commit from CodeProject, Added a namespace and solution

---
+++ RP.Math.Vector3/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RP.Math.Vector3")]
+[assembly: AssemblyDescription("A Vector Type for C#")]
+[assembly: AssemblyProduct("RP.Math.Vector3")]
+[assembly: AssemblyCopyright("Copyright ©  2004")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6963a3a8-8620-49dd-a117-90ee7a68d330")]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
