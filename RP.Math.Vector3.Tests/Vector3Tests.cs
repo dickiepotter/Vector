8e8dc1591e4f11a85e5bc235364263d774011676
Richard Potter <dickiepotter@hotmail.com>
2015-01-15 10:32:16 +0000

    Added Unit Test project

---
+++ RP.Math.Vector3.Tests/Vector3Tests.cs
@@ -0,0 +1,39 @@
+﻿namespace RP.Math.Tests
+{
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Vector3Tests
+    {
+        [TestMethod]
+        public void ConstructionWithXYZParametersTest()
+        {
+            var vector = new Vector3(100, -100, 0.01);
+            vector.X.Should().Be(100d);
+            vector.Y.Should().Be(-100d);
+            vector.Z.Should().Be(0.01d);
+        }
+
+        [TestMethod]
+        public void ConstructionWithAVector3ParameterTest()
+        { 
+            var vector = new Vector3( new Vector3(100, -100, 0));
+
+            vector.X.Should().Be(100);
+            vector.Y.Should().Be(-100);
+            vector.Z.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ConstructorWithAnArrayOfXYZParameterTest()
+        {
+            var vector = new Vector3(new[] { 100, -100, 0.01 });
+
+            vector.X.Should().Be(100);
+            vector.Y.Should().Be(-100);
+            vector.Z.Should().Be(0.01);
+        }
+    }
+}
