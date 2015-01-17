0c3fc0f6023c8b38cb5e67fd320193599f1e3224
Richard Potter <dickiepotter@hotmail.com>
2015-01-17 12:05:08 +0000

    Added Unit Tests for Sqr, Sqrr

--- RP.Math.Vector3.Tests/Vector3Tests.cs
+++ RP.Math.Vector3.Tests/Vector3Tests.cs
@@ -7,6 +7,8 @@
     [TestClass]
     public class Vector3Tests
     {
+        #region Constructor tests
+
         [TestMethod]
         public void ConstructionWithXYZParametersTest()
         {
@@ -35,5 +37,43 @@
             vector.Y.Should().Be(-100);
             vector.Z.Should().Be(0.01);
         }
+
+        [TestMethod]
+        public void ConstructorWithNoParametersTest()
+        {
+            var vector = new Vector3();
+            
+            vector.X.Should().Be(0);
+            vector.Y.Should().Be(0);
+            vector.Z.Should().Be(0);
+        }
+
+        #endregion
+
+        #region Square and Square-Root Tests
+
+        [TestMethod]
+        public void SquareComponentsTest()
+        {
+            var vector = new Vector3(2, 3, 4);
+            vector.SqrComponents();
+
+            vector.X.Should().Be(4);
+            vector.Y.Should().Be(9);
+            vector.Z.Should().Be(16);
+        }
+
+        [TestMethod]
+        public void SquareRootComponentsTest()
+        {
+            var vector = new Vector3(4, 9, 16);
+            vector.SqrtComponents();
+
+            vector.X.Should().Be(2);
+            vector.Y.Should().Be(3);
+            vector.Z.Should().Be(4);
+        }
+
+        #endregion
     }
 }
