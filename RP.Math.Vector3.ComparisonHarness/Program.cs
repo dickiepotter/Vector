999adeb52227ab452928ef0a3aa2aaf74271b185
Richard Potter <dickiepotter@hotmail.com>
2015-01-22 11:17:01 +0000

    Added further comparisons

--- RP.Math.Vector3.ComparisonHarness/Program.cs
+++ RP.Math.Vector3.ComparisonHarness/Program.cs
@@ -10,6 +10,14 @@
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Test Utility for RP.Math.Vector3");
+            Console.WriteLine();
+            Console.WriteLine(@"This utility will test various vector operations on RP.Math.Vector3 and a number of third party libraries.");
+            Console.WriteLine(@"The result is time taken for {0} iterations of the operation in ticks.", iterations);
+            Console.WriteLine();
+            Console.WriteLine("===========================================");
+            Console.WriteLine();
+
             var results = RunTests();
 
             foreach (var kvp in results)
@@ -17,8 +25,13 @@
                 Console.WriteLine(kvp.Value + " => " + kvp.Key);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("=================================================");
+            Console.WriteLine(" Done... Press Any Key to Exit");
             Console.ReadKey();
         }
+
+        #region Helper Methods
 
         private static long Measure(int iterations, Action action)
         {
@@ -33,188 +46,323 @@
             return stopwatch.ElapsedTicks;
         }
 
+        #endregion
+
+        #region Run the tests
+
         private static Dictionary<string, long> RunTests()
         {
             var results = new Dictionary<string, long>();
 
-            #region RP.Math.Vector3
+            RunVector3Tests(results);
+            RunMathNetTests(results);
+            RunSharpDxTests(results);
+            RunSevenTests(results);
+            RunExocortexTests(results);
+
+            return results;
+        }
+
+        /// <summary>
+        /// Run tests for SharpDx
+        /// </summary>
+        /// <seealso cref="http://sharpdx.org/"/>
+        /// <param name="results">The collection of results to add to</param>
+        /// <remarks>Included using Nuget</remarks>
+        private static void RunSharpDxTests(Dictionary<string, long> results)
+        {
+            var libraryName = "SharpDX";
+
+            var a = new SharpDX.Vector3(2, 4, 6);
+            var b = new SharpDX.Vector3(5, 7, 10);
 
             results.Add(
-                "Cross Product => RP.Math.Vector3",
+                "Cross Product => " + libraryName,
                 Measure(
                     iterations,
                     () =>
                         {
-                            var a = new Vector3(2, 4, 6);
-                            var b = new Vector3(5, 7, 10);
-                            var result = a.CrossProduct(b);
+                            var result = SharpDX.Vector3.Cross(a, b);
                         }));
 
             results.Add(
-                "Dot Product => RP.Math.Vector3",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new Vector3(2, 4, 6);
-                        var b = new Vector3(5, 7, 10);
-                        var result = a.DotProduct(b);
-                    }));
-
-            results.Add(
-                "Normalize => RP.Math.Vector3",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new Vector3(2, 4, 6);
-                        var b = new Vector3(5, 7, 10);
-                        var result = a.Normalize();
-                    }));
-
-            results.Add(
-                "Add => RP.Math.Vector3",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new Vector3(2, 4, 6);
-                        var b = new Vector3(5, 7, 10);
-                        var result = a + b;
-                    }));
-
-            results.Add(
-               "Subtract => RP.Math.Vector3",
-               Measure(
-                   iterations,
-                   () =>
-                   {
-                       var a = new Vector3(2, 4, 6);
-                       var b = new Vector3(5, 7, 10);
-                       var result = a - b;
-                   }));
-
-            #endregion
-
-            #region Math.Net
-
-            results.Add(
-                "Cross Product => MathNet",
+                "Dot Product => " + libraryName,
                 Measure(
                     iterations,
                     () =>
                         {
-                            var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 2, 4, 6 });
-                            var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 5, 7, 10 });
+                            var result = SharpDX.Vector3.Dot(a, b);
+                        }));
+
+            results.Add(
+                "Normalize => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            // Mutable
+                            var vector = new SharpDX.Vector3(2, 4, 6);
+                            vector.Normalize();
+                        }));
+
+            results.Add(
+                "Add => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a + b;
+                        }));
+
+            results.Add(
+                "Subtract => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a - b;
+                        }));
+        }
+
+        /// <summary>
+        /// Run tests for Math.Net
+        /// </summary>
+        /// <seealso cref="http://www.mathdotnet.com/"/>
+        /// <param name="results">The collection of results to add to</param>
+        /// <remarks>Included using Nuget</remarks>
+        private static void RunMathNetTests(Dictionary<string, long> results)
+        {
+            var libraryName = "MathNet";
+
+            var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 2, 4, 6 });
+            var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 5, 7, 10 });
+
+            results.Add(
+                "Cross Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
                             var result = a.OuterProduct(b); // Not sure if this is cross product or not
                         }));
 
             results.Add(
-                "Dot Product => MathNet",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 2, 4, 6 });
-                        var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 5, 7, 10 });
-                        var result = a.DotProduct(b);
-                    }));
-
-            results.Add(
-                "Normalize => MathNet",
+                "Dot Product => " + libraryName,
                 Measure(
                     iterations,
                     () =>
                         {
-                            var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 2, 4, 6 });
-                            var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 5, 7, 10 });
+                            var result = a.DotProduct(b);
+                        }));
+
+            results.Add(
+                "Normalize => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
                             var result = a.Normalize(0);
-                    }));
+                        }));
 
             results.Add(
-                "Add => MathNet",
+                "Add => " + libraryName,
                 Measure(
                     iterations,
                     () =>
-                    {
-                        var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] {2, 4, 6});
-                        var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] {5, 7, 10});
-                        var result = a + b;
-                    }));
+                        {
+                            var result = a + b;
+                        }));
 
             results.Add(
-               "Subtract => MathNet",
-               Measure(
-                   iterations,
-                   () =>
-                   {
-                       var a = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] {2, 4, 6});
-                       var b = new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] {5, 7, 10});
-                       var result = a - b;
-                   }));
-
-            #endregion
-
-            #region SharpDX
-
-            results.Add(
-                "Cross Product => SharpDX",
+                "Subtract => " + libraryName,
                 Measure(
                     iterations,
                     () =>
-                    {
-                        var a = new SharpDX.Vector3(2, 4, 6);
-                        var b = new SharpDX.Vector3(5, 7, 10);
-                        var result = SharpDX.Vector3.Cross(a, b);
-                    }));
-
-            results.Add(
-                "Dot Product => SharpDX",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new SharpDX.Vector3(2, 4, 6);
-                        var b = new SharpDX.Vector3(5, 7, 10);
-                        var result = SharpDX.Vector3.Dot(a, b);
-                    }));
-
-            results.Add(
-                "Normalize => SharpDX",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new SharpDX.Vector3(2, 4, 6);
-                        var b = new SharpDX.Vector3(5, 7, 10);
-                        a.Normalize();
-                    }));
-
-            results.Add(
-                "Add => SharpDX",
-                Measure(
-                    iterations,
-                    () =>
-                    {
-                        var a = new SharpDX.Vector3(2, 4, 6);
-                        var b = new SharpDX.Vector3(5, 7, 10);
-                        var result = a + b;
-                    }));
-
-            results.Add(
-               "Subtract => SharpDX",
-               Measure(
-                   iterations,
-                   () =>
-                   {
-                       var a = new SharpDX.Vector3(2, 4, 6);
-                       var b = new SharpDX.Vector3(5, 7, 10);
-                       var result = a - b;
-                   }));
-
-            #endregion
-
-            return results;
+                        {
+                            var result = a - b;
+                        }));
         }
+
+        /// <summary>
+        /// Run tests for Seven Framework (From comments on <seealso cref="http://www.codeproject.com/Articles/17425/A-Vector-Type-for-C"/>
+        /// </summary>
+        /// <seealso cref="https://github.com/53V3N1X/SevenFramework"/>
+        /// <param name="results">The collection of results to add to</param>
+        /// <remarks>Included as a DLL</remarks>
+        private static void RunSevenTests(Dictionary<string, long> results)
+        {
+            var libraryName = "Seven";
+
+            var a = new Seven.Mathematics.Vector<double>(2d, 4d, 6d);
+            var b = new Seven.Mathematics.Vector<double>(5d, 7d, 10d);
+
+            results.Add(
+                "Cross Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a.CrossProduct(b);
+                    }));
+
+            results.Add(
+                "Dot Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a.DotProduct(b);
+                    }));
+
+            results.Add(
+                "Normalize => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a.Normalize();
+                    }));
+
+            results.Add(
+                "Add => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a + b;
+                    }));
+
+            results.Add(
+                "Subtract => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a - b;
+                    }));
+        }
+
+        /// <summary>
+        /// Run tests for ExoCortex
+        /// </summary>
+        /// <seealso cref="http://www.codeproject.com/Articles/1728/ExoEngine-a-C-D-engine"/>
+        /// <param name="results">The collection of results to add to</param>
+        /// <remarks>
+        /// Included as a DLL.
+        /// The vector implementation in this library only comes as a container of float data types (the test may not be strictly fair)
+        /// </remarks>
+        private static void RunExocortexTests(Dictionary<string, long> results)
+        {
+            var libraryName = "Exocortex";
+
+            var a = new Exocortex.Geometry3D.Vector3D(2f, 4f, 6f);
+            var b = new Exocortex.Geometry3D.Vector3D(5f, 7f, 10f);
+
+            results.Add(
+                "Cross Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = Exocortex.Geometry3D.Vector3D.CrossProduct(a, b);
+                    }));
+
+            results.Add(
+                "Dot Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = Exocortex.Geometry3D.Vector3D.DotProduct(a, b);
+                    }));
+
+            results.Add(
+                "Normalize => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        // Mutable
+                        var vector = new Exocortex.Geometry3D.Vector3D(2f, 4f, 6f);
+                        vector.Normalize();
+                    }));
+
+            results.Add(
+                "Add => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a + b;
+                    }));
+
+            results.Add(
+                "Subtract => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                    {
+                        var result = a - b;
+                    }));
+        }
+
+        /// <summary>
+        /// Run tests for this Vector3 project
+        /// </summary>
+        /// <param name="results">The collection of results to add to</param>
+        private static void RunVector3Tests(Dictionary<string, long> results)
+        {
+            var libraryName = "RP.Math.Vector3";
+
+            var a = new Vector3(2, 4, 6);
+            var b = new Vector3(5, 7, 10);
+
+            results.Add(
+                "Cross Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a.CrossProduct(b);
+                        }));
+
+            results.Add(
+                "Dot Product => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a.DotProduct(b);
+                        }));
+
+            results.Add(
+                "Normalize => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a.Normalize();
+                        }));
+
+            results.Add(
+                "Add => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a + b;
+                        }));
+
+            results.Add(
+                "Subtract => " + libraryName,
+                Measure(
+                    iterations,
+                    () =>
+                        {
+                            var result = a - b;
+                        }));
+        }
+
+        #endregion
     }
 }
