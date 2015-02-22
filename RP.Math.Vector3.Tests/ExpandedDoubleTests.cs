7f2c917a456c70ede45237d69b6c60f7f0d3558e
Richard Potter <dickiepotter@hotmail.com>
2015-02-22 12:43:01 +0000

    Non-Working, Ignored unit test for ExpandedDouble struct that needs work.

---
+++ RP.Math.Vector3.Tests/ExpandedDoubleTests.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExpandedDoubleTests.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ExpandedDoubleTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RP.Math.Tests
+{
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExpandedDoubleTests
+    {
+        #region Construction Tests
+
+        // TODO Need a better understanding of the underlying structure of doubles, this test is plain wrong
+        [TestMethod, TestCategory("Construction"), Ignore]
+        public void Construction_WhereWholeNumberDouble_ShouldSplitCorrectly_Test()
+        {
+            double d = 100;
+            var result = new ExpandedDouble(d);
+
+            result.IsNegative.Should().Be(false, "100.5 is a positive number");
+            result.Exponent.Should().Be(1, "the exponent of 100.5 is 1");
+            result.Mantissa.Should().Be(1005, "the mantissa of 100.5 is 1005");
+        }
+
+        #endregion
+    }
+}
