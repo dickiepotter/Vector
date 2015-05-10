850a1c3bf8ed0ab1d00a8fd550ee3437880fad3e
Richard Potter <dickiepotter@hotmail.com>
2015-05-10 13:55:10 +0100

    Assembly Info Updates

--- RP.Math.Vector3.Tests/Properties/AssemblyInfo.cs
+++ RP.Math.Vector3.Tests/Properties/AssemblyInfo.cs
@@ -4,19 +4,18 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-// System.Security
+[assembly: AssemblyTitle("RP.Math.Vector3.Tests")]
+[assembly: AssemblyDescription("Vector3 Unit Tests")]
+[assembly: AssemblyProduct("RP.Math.Vector3")]
+[assembly: AssemblyCopyright("Copyright Richard Potter 2004")]
+
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
 
-// System.Reflection
-[assembly: AssemblyTitle("RP.Math.Vector3 Pex Tests")]
-
-// System.Runtime.InteropServices
 [assembly: ComVisible(false)]
+
 [assembly: Guid("5ff9ac98-e109-4f95-ab5c-9335aee47e4d")]
 
-// System
 [assembly: CLSCompliant(false)]
 
-// System.Runtime.CompilerServices
 [assembly: InternalsVisibleTo("Microsoft.Pex, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
 
