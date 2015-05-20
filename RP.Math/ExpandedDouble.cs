edf2d0e2b5adabec27aba80c7b7b1f7c2977d13e
Richard Potter <dickiepotter@hotmail.com>
2015-05-20 18:46:34 +0100

    Removed references to pex and hopefully fixed the nuget references

--- RP.Math/ExpandedDouble.cs
+++ RP.Math/ExpandedDouble.cs
@@ -2,7 +2,6 @@ namespace RP.Math
 {
     using System;
     using System.Collections.Generic;
-    using System.Numerics;
     using System.Runtime.InteropServices.WindowsRuntime;
 
     /// <summary>
