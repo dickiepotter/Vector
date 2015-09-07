faca8ff59ac8100a00baa662ebec44c4f1f2afde
Richard Potter <dickiepotter@hotmail.com>
2015-09-07 09:07:33 +0100

    SharpDX and OpenTK examples working but not using Vector3 yet.

---
+++ RP.Math.Demo.OpenTK/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTKCube
+{
+    class Program : GameWindow
+    {
+        #region Cube information
+
+        float[] cubeColors = {
+			1.0f, 0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 1.0f, 1.0f, 1.0f,
+			1.0f, 0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 1.0f, 1.0f, 1.0f,
+		};
+
+        byte[] triangles =
+		{
+			1, 0, 2, // front
+			3, 2, 0,
+			6, 4, 5, // back
+			4, 6, 7,
+			4, 7, 0, // left
+			7, 3, 0,
+			1, 2, 5, //right
+			2, 6, 5,
+			0, 1, 5, // top
+			0, 5, 4,
+			2, 3, 6, // bottom
+			3, 7, 6,
+		};
+
+        float[] cube = {
+			-0.5f,  0.5f,  0.5f, // vertex[0]
+			 0.5f,  0.5f,  0.5f, // vertex[1]
+			 0.5f, -0.5f,  0.5f, // vertex[2]
+			-0.5f, -0.5f,  0.5f, // vertex[3]
+			-0.5f,  0.5f, -0.5f, // vertex[4]
+			 0.5f,  0.5f, -0.5f, // vertex[5]
+			 0.5f, -0.5f, -0.5f, // vertex[6]
+			-0.5f, -0.5f, -0.5f, // vertex[7]
+		};
+
+        #endregion
+
+        Matrix4 matrixProjection, matrixModelview;
+        float cameraRotation = 0f;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.CullFace);
+            GL.EnableClientState(EnableCap.VertexArray);
+            GL.EnableClientState(EnableCap.ColorArray);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            matrixProjection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1f, 100f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref matrixProjection);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            #region Camera
+
+            cameraRotation = (cameraRotation < 360f) ? (cameraRotation + 1f * (float)e.Time) : 0f;
+            Matrix4.CreateRotationY(cameraRotation, out matrixModelview);
+            matrixModelview *= Matrix4.LookAt(0f, 0f, -5f, 0f, 0f, 0f, 0f, 1f, 0f);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref matrixModelview);
+
+            #endregion
+
+            #region Draw cube
+
+            GL.VertexPointer(3, VertexPointerType.Float, 0, cube);
+            GL.ColorPointer(4, ColorPointerType.Float, 0, cubeColors);
+            GL.DrawElements(BeginMode.Triangles, 36, DrawElementsType.UnsignedByte, triangles);
+
+            #endregion
+
+            SwapBuffers();
+        }
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            using (Program p = new Program())
+            {
+                p.Run(60d);
+            }
+        }
+
+    }
+}
--- RP.Math.Demo.SharpDx/Program.cs
+++ RP.Math.Demo.SharpDx/Program.cs
@@ -1,102 +1,202 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
 
-namespace RP.Math.Demo.SharpDx
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+
+namespace MiniCube
 {
-    using System.Drawing;
-
-    using SharpDX;
-    using SharpDX.Direct3D9;
-    using SharpDX.Windows;
-
-    class Program
+    /// <summary>
+    /// An example spinning cube for SharpDx
+    /// </summary>
+    internal static class Program
     {
-        #region Entry Point to Application
-
-        /// <summary>
-        /// The entry point for the application
-        /// </summary>
-        /// <param name="args">
-        /// The commandline arguments.
-        /// </param>
-        public static void Main(string[] args)
+        private static void Main()
         {
-            // Create Render Form
-            Initialize();
+            var form = new RenderForm("SharpDX - Cube");
 
-            // Run the drawing loop
-            RenderLoop.Run(form, Draw);
-        }
-
-        #endregion
-
-        #region Drawing Loop
-
-        /// <summary>
-        /// The drawing loop where we put thing on screen each time the form renders
-        /// </summary>
-        private static void Draw()
-        {
-            device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, SharpDX.Color.Black, 1.0f, 0);
-            device.BeginScene();
-
-            DrawCube();
-
-            device.EndScene();
-            device.Present();
-        }
-
-        #endregion
-
-        #region Initialize
-
-        /// <summary>
-        /// The form on which rendering will be done
-        /// </summary>
-        private static RenderForm form;
-
-        /// <summary>
-        /// Initial form width
-        /// </summary>
-        private const int Width = 800;
-
-        /// <summary>
-        /// Initial form height
-        /// </summary>
-        private const int Height = 600;
-
-        /// <summary>
-        /// The DirectX device configuration
-        /// </summary>
-        private static Device device;
-
-
-        // Create the Render form and DirectX device
-        private static void Initialize()
-        {
-            form = new RenderForm("ShardDX Demo")
+            // SwapChain description
+            var desc = new SwapChainDescription()
             {
-                ClientSize = new Size(Width, Height)
+                BufferCount = 1,
+                ModeDescription =
+                    new ModeDescription(form.ClientSize.Width, form.ClientSize.Height, new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription(1, 0),
+                SwapEffect = SwapEffect.Discard,
+                Usage = Usage.RenderTargetOutput
             };
 
-            var direct3d = new Direct3D();
+            // Create Device and SwapChain
+            Device device;
+            SwapChain swapChain;
+            Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.Debug, desc, out device, out swapChain);
+            var context = device.ImmediateContext;
 
-            var presentation = new PresentParameters(form.ClientSize.Width, form.ClientSize.Height);
+            // Ignore all windows events
+            var factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll);
 
-            device = new Device(direct3d, 0, DeviceType.Hardware, form.Handle, CreateFlags.HardwareVertexProcessing, presentation);
+            // Compile Vertex and Pixel shaders
+            var vertexShaderByteCode = ShaderBytecode.CompileFromFile("Cube.fx", "VS", "vs_4_0");
+            var vertexShader = new VertexShader(device, vertexShaderByteCode);
+
+            var pixelShaderByteCode = ShaderBytecode.CompileFromFile("Cube.fx", "PS", "ps_4_0");
+            var pixelShader = new PixelShader(device, pixelShaderByteCode);
+
+            var signature = ShaderSignature.GetInputSignature(vertexShaderByteCode);
+            // Layout from VertexShader input signature
+            var layout = new InputLayout(device, signature, new[]
+                    {
+                        new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0),
+                        new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
+                    });
+
+            // Instantiate Vertex buiffer from vertex data
+            var vertices = Buffer.Create(device, BindFlags.VertexBuffer, GetVectors());
+
+            // Create Constant Buffer
+            var contantBuffer = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+
+            // Prepare All the stages
+            context.InputAssembler.InputLayout = layout;
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertices, Utilities.SizeOf<Vector4>() * 2, 0));
+            context.VertexShader.SetConstantBuffer(0, contantBuffer);
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+            // Prepare matrices
+            var view = Matrix.LookAtLH(new Vector3(0, 0, -5), new Vector3(0, 0, 0), Vector3.UnitY);
+            Matrix proj = Matrix.Identity;
+
+            // Use clock
+            var clock = new Stopwatch();
+            clock.Start();
+
+            // Declare texture for rendering
+            bool userResized = true;
+            Texture2D backBuffer = null;
+            RenderTargetView renderView = null;
+            Texture2D depthBuffer = null;
+            DepthStencilView depthView = null;
+
+            // Setup handler on resize form
+            form.UserResized += (sender, args) => userResized = true;
+
+            // Setup full screen mode change F5 (Full) F4 (Window)
+            form.KeyUp += (sender, args) =>
+            {
+                if (args.KeyCode == Keys.F5)
+                    swapChain.SetFullscreenState(true, null);
+                else if (args.KeyCode == Keys.F4)
+                    swapChain.SetFullscreenState(false, null);
+                else if (args.KeyCode == Keys.Escape)
+                    form.Close();
+            };
+
+            // Main loop
+            RenderLoop.Run(form, () =>
+            {
+                // If Form resized
+                if (userResized)
+                {
+                    // Dispose all previous allocated resources
+                    Utilities.Dispose(ref backBuffer);
+                    Utilities.Dispose(ref renderView);
+                    Utilities.Dispose(ref depthBuffer);
+                    Utilities.Dispose(ref depthView);
+
+                    // Resize the backbuffer
+                    swapChain.ResizeBuffers(desc.BufferCount, form.ClientSize.Width, form.ClientSize.Height, Format.Unknown, SwapChainFlags.None);
+
+                    // Get the backbuffer from the swapchain
+                    backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+
+                    // Renderview on the backbuffer
+                    renderView = new RenderTargetView(device, backBuffer);
+
+                    // Create the depth buffer
+                    depthBuffer = new Texture2D(device, new Texture2DDescription()
+                    {
+                        Format = Format.D32_Float_S8X24_UInt,
+                        ArraySize = 1,
+                        MipLevels = 1,
+                        Width = form.ClientSize.Width,
+                        Height = form.ClientSize.Height,
+                        SampleDescription = new SampleDescription(1, 0),
+                        Usage = ResourceUsage.Default,
+                        BindFlags = BindFlags.DepthStencil,
+                        CpuAccessFlags = CpuAccessFlags.None,
+                        OptionFlags = ResourceOptionFlags.None
+                    });
+
+                    // Create the depth buffer view
+                    depthView = new DepthStencilView(device, depthBuffer);
+
+                    // Setup targets and viewport for rendering
+                    context.Rasterizer.SetViewport(new Viewport(0, 0, form.ClientSize.Width, form.ClientSize.Height, 0.0f, 1.0f));
+                    context.OutputMerger.SetTargets(depthView, renderView);
+
+                    // Setup new projection matrix with correct aspect ratio
+                    proj = Matrix.PerspectiveFovLH((float)Math.PI / 4.0f, form.ClientSize.Width / (float)form.ClientSize.Height, 0.1f, 100.0f);
+
+                    // We are done resizing
+                    userResized = false;
+                }
+
+                var time = clock.ElapsedMilliseconds / 1000.0f;
+
+                var viewProj = Matrix.Multiply(view, proj);
+
+                // Clear views
+                context.ClearDepthStencilView(depthView, DepthStencilClearFlags.Depth, 1.0f, 0);
+                context.ClearRenderTargetView(renderView, Color.Black);
+
+                // Update WorldViewProj Matrix
+                var worldViewProj = Matrix.RotationX(time) * Matrix.RotationY(time * 2) * Matrix.RotationZ(time * .7f) * viewProj;
+                worldViewProj.Transpose();
+                context.UpdateSubresource(ref worldViewProj, contantBuffer);
+
+                // Draw the cube
+                context.Draw(36, 0);
+
+                // Present!
+                swapChain.Present(0, PresentFlags.None);
+            });
+
+            // Release all resources
+            signature.Dispose();
+            vertexShaderByteCode.Dispose();
+            vertexShader.Dispose();
+            pixelShaderByteCode.Dispose();
+            pixelShader.Dispose();
+            vertices.Dispose();
+            layout.Dispose();
+            contantBuffer.Dispose();
+            depthBuffer.Dispose();
+            depthView.Dispose();
+            renderView.Dispose();
+            backBuffer.Dispose();
+            context.ClearState();
+            context.Flush();
+            device.Dispose();
+            context.Dispose();
+            swapChain.Dispose();
+            factory.Dispose();
         }
 
-        #endregion
-
-        #region Cube
-
-        private static void DrawCube()
+        private static Vector4[] GetVectors()
         {
-            var vertices = new VertexBuffer(device, Utilities.SizeOf<Vector4>() * 2 * 36, Usage.WriteOnly, VertexFormat.None, Pool.Managed);
-            vertices.Lock(0, 0, LockFlags.None).WriteRange(new[]
+            return new[]
             {
                 new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front
                 new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
@@ -139,25 +239,7 @@ namespace RP.Math.Demo.SharpDx
                 new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
                 new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
                 new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
-            });
-            vertices.Unlock();
-
-            // Allocate Vertex Elements
-            var vertexElems = new[] {
-        		new VertexElement(0, 0, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.Position, 0),
-        		new VertexElement(0, 16, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.Color, 0),
-				VertexElement.VertexDeclarationEnd
-        	};
-
-            // Creates and sets the Vertex Declaration
-            var vertexDecl = new VertexDeclaration(device, vertexElems);
-            device.SetStreamSource(0, vertices, 0, Utilities.SizeOf<Vector4>() * 2);
-            device.VertexDeclaration = vertexDecl;
-
-            // Draw the cube
-            device.DrawPrimitives(PrimitiveType.TriangleList, 0, 12);
+            };
         }
-
-        #endregion
     }
 }
